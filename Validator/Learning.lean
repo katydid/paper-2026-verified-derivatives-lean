a25c8d0f11a33eb52712c9b0ce2d8f1883f1bb00
Walter Schulze <awalterschulze@gmail.com>
2025-12-31 16:18:29 +0000

    delete Original and Compress

--- Validator/Learning.lean
+++ Validator/Learning.lean
@@ -1,6 +1,4 @@
 import Validator.Learning.Basic
-import Validator.Learning.Compress
 import Validator.Learning.Memoize
-import Validator.Learning.Original
 import Validator.Learning.OriginalTotal
 import Validator.Learning.Parser
